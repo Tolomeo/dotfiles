5c73a636a2656fde95c75eee215872b88f292b21
Diego Frattini <dgfrattini@gmail.com>
2023-12-22 12:06:18 +0000

    Zsh as linux default shell

--- post-install.sh
+++ post-install.sh
@@ -6,17 +6,26 @@ else
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
+
 echo 'Installing system packages'
 brew bundle --file=~/.config/brewfile/Brewfile
 
 echo 'Installing node lts'
-export NVM_DIR="$HOME/.nvm"
-[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
 nvm install --lts
 
 if [ ! "$(basename "$SHELL")" = "zsh" ]; then
-	echo 'Changing default shell to zsh. You will probably have to logout and log back in'
+	echo 'Changing default shell to Zsh. You will probably have to logout and log back in'
 	chsh -s "$(which zsh)" "$(whoami)"
+
+	# Append the zsh path to the shells file
+	if grep -qx "$(which zsh)" "/etc/shells"; then
+		echo "Zsh is in the shells file"
+	else
+		echo "$(which zsh)" | tee -a "/etc/shells" >/dev/null
+		echo "Zsh path added to the shells file"
+	fi
 else
 	echo 'Using zsh as default shell'
 fi
