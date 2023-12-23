9cb9d4874e6b0dc0e7734fc211f5e13548870ff7
Diego Frattini <dgfrattini@gmail.com>
2023-12-23 22:28:59 +0000

    Homebrew env to homebrew dir

--- homebrew/env.sh
+++ homebrew/env.sh
@@ -4,7 +4,11 @@ else
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-# Nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
-[ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"
+
+if [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ]; then
+	. "$(brew --prefix)/opt/nvm/nvm.sh"
+fi
+# if [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ]; then
+# 	. "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"
+# fi
