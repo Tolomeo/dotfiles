d635d6897fc210a4bfb1c21452ed81c16413800c
Diego Frattini <dgfrattini@gmail.com>
2023-12-23 21:47:51 +0000

    Post-install removal

--- post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sh ~/.dotfiles/scripts/homebrew_install.sh
-
-sh ~/.dotfiles/scripts/node_install.sh
-
-sh ~/.dotfiles/scripts/zsh_install.sh
-
-sh ~/.dotfiles/scripts/fonts_install.sh
