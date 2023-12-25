0e4ee4e3ebfd0490f171210f3465616f7bf77b0e
Diego Frattini <dgfrattini@gmail.com>
2023-12-25 18:51:45 +0000

    System deps install

---
+++ scripts/deps_install.sh
@@ -0,0 +1,26 @@
+case "$(uname)" in
+Darwin)
+	if ! xcode-select -p >/dev/null 2>&1; then
+		echo "Installing Xcode Command Line Tools"
+
+		xcode-select --install &
+		wait $!
+
+		if xcode-select -p >/dev/null 2>&1; then
+			echo "Xcode Command Line Tools installed successfully"
+		else
+			echo "Xcode Command Line Tools installation failed"
+			exit 1
+		fi
+	fi
+	;;
+Linux)
+	echo "Installing system dependencies"
+
+	deps="build-essential procps curl file"
+
+	for dep in $deps; do
+		sudo apt install -y $dep
+	done
+	;;
+esac
