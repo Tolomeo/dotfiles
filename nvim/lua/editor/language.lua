60ac3bde48b6e28a8d347fca522b0e41dc946921
Diego Frattini <dgfrattini@gmail.com>
2025-10-13 17:06:58 +0100

    Nvim lspconfig to vim.lsp migration

--- nvim/lua/editor/language.lua
+++ nvim/lua/editor/language.lua
@@ -195,8 +195,8 @@ function Language:setup_servers()
 	require("neodev").setup()
 
 	for language_server_name, language_server_setup in pairs(language_server_setups) do
-		require('lspconfig')[language_server_name].setup(language_server_setup)
-		-- vim.lsp.config(language_server_name, language_server_setup)
+			vim.lsp.config(language_server_name, language_server_setup)
+			vim.lsp.enable(language_server_name)
 	end
 
 	-- Diagnostic signs
